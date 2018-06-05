306b738e837f3867aa2b373e72b442beabd5b092
ry00001 <15631667+ry00001@users.noreply.github.com>
2018-06-05 18:47:28 +0100

    this is so broken

--- commands/link.js
+++ commands/link.js
@@ -6,7 +6,7 @@ module.exports = {
         if (args[0] === 'add' && hasPerm(ctx.member)) {
             if (!args[1]) return await ctx.send(`Syntax: \`${ctx.prefix}${ctx.command.name} add <link name> <contents>\``)
             if (await ctx.bot.db[ctx.guild.id].tags[args[1]].exists()) return await ctx.send('This link already exists.')
-            await ctx.bot.db[ctx.guild.id].tags[args[1]].set(args.slice(1, args.length))
+            await ctx.bot.db[ctx.guild.id].tags[args[1]].set(args.slice(2, args.length))
             return await ctx.send('Link added.')
         }
         if (args[0] === 'rm' && hasPerm(ctx.member)) {
@@ -18,7 +18,7 @@ module.exports = {
         if (!args[0]) return await ctx.send(`Syntax: \`${ctx.prefix}${ctx.command.name} <link name>\``)
         if (!(await ctx.bot.db[ctx.guild.id].tags[args[0]].exists())) return await ctx.send('This link doesn\'t exist.')
         let link = await ctx.bot.db[ctx.guild.id].tags[args[0]].get
-        await ctx.send(link)
+        await ctx.send(link.join(' '))
     },
     aliases: ['l']
 }
