31d5c19e61e55a470301f6bcd9ec1f61ff78920b
ry00001 <15631667+ry00001@users.noreply.github.com>
2018-06-05 18:49:31 +0100

    FINAL FINAL update

--- commands/link.js
+++ commands/link.js
@@ -12,8 +12,10 @@ module.exports = {
         if (args[0] === 'rm' && hasPerm(ctx.member)) {
             if (!args[1]) return await ctx.send(`Syntax: \`${ctx.prefix}${ctx.command.name} rm <link name>\``)
             if (!(await ctx.bot.db[ctx.guild.id].tags[args[1]].exists())) return await ctx.send('This link doesn\'t exist.')
-            await ctx.bot.db[ctx.guild.id].tags[args[1]].delete
-            return await ctx.send('Link added.')
+            let alllinks = await ctx.bot.db[ctx.guild.id].tags.get
+            delete alllinks[args[1]]
+            await ctx.bot.db[ctx.guild.id].tags.set(alllinks)
+            return await ctx.send('Link deleted.')
         }
         if (!args[0]) return await ctx.send(`Syntax: \`${ctx.prefix}${ctx.command.name} <link name>\``)
         if (!(await ctx.bot.db[ctx.guild.id].tags[args[0]].exists())) return await ctx.send('This link doesn\'t exist.')
